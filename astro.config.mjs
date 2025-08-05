3f618f980d2c904956fd54f7abb163ff716d69c6
Murat Polat Ozkan <muratpolatozkan@gmail.com>
2025-08-05 12:41:33 +0300

    feat: navbar and megaSubmenu

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,16 @@ import tailwindcss from "@tailwindcss/vite";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
+import lit from "@astrojs/lit";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://plusui.com",
-  integrations: [mdx(), sitemap(), icon()],
+  integrations: [mdx(), sitemap(), icon(), lit()],
   vite: {
     plugins: [tailwindcss()],
+    ssr: {
+      noExternal: ["@plusui/library/cdn/components/index.js"],
+    },
   },
 });
